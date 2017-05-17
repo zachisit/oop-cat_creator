89b3ce18e96ba1931ae950c5835de92184c829f1
Zachary Smith <info@zachis.it>
2017-05-17 09:32:58 -0400

    updated readme file

--- index.php
+++ index.php
@@ -8,3 +8,5 @@ require_once "classCat.php";
 $tater = new Cat("Tater B. Arfoluem");
 
 $rex = new Cat();
+
+echo '<center>project sourcecode can be found at <a href="https://github.com/zachisit/oop-cat_creator" title="github" target="_blank">github.com/zachisit/oop-cat_creator</a></center>';
